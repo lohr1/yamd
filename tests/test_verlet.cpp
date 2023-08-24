5fe60fb094412e0156942f7dc344a5a98ad8b495
Robin <rob.loh32@gmail.com>
2023-08-24 11:40:13 +0200

    Fixed verlet test

--- tests/test_verlet.cpp
+++ tests/test_verlet.cpp
@@ -1,39 +1,34 @@
-//
-// Created by robin on 10.05.23.
-//
-// Include a library file to make sure proper includes are set
+/*
+ * Test file for verlet integrator of single and multiple atoms.
+ */
 #include "verlet.h"
 #include "types.h"
 #include <gtest/gtest.h>
 #include <vector>
 
-// Demonstrate some basic assertions.
-TEST(VerletTest, AnalyticalNearness) {
-    // Initialize pos, vel for single atom
-    // using random double in [-1, 1]
+// Single atom test
+TEST(VerletTest, SingleAtom) {
+    srand((unsigned int) time(0)); // Seed random num generator
+
+    // Initialize pos, vel, and forces for single atom
+    // using random doubles in [-1, 1]
     double x = std::rand() * 2.0 / RAND_MAX - 1;
     double y = std::rand() * 2.0 / RAND_MAX - 1;
     double z = std::rand() * 2.0 / RAND_MAX - 1;
     double vx = std::rand() * 2.0 / RAND_MAX - 1;
     double vy = std::rand() * 2.0 / RAND_MAX - 1;
     double vz = std::rand() * 2.0 / RAND_MAX - 1;
+    double fx = std::rand() * 2.0 / RAND_MAX - 1;
+    double fy = std::rand() * 2.0 / RAND_MAX - 1;
+    double fz = std::rand() * 2.0 / RAND_MAX - 1;
 
-    x = 0;
-    y = 0;
-    z = 0;
-    vx = 0;
-    vy = 0;
-    vz = 0;
-    // Save for later
+    // Save initials for analysis (force is constant)
     std::vector<double> init_pos = {x, y, z};
-    std::vector<double> init_vel = {x, y, z};
+    std::vector<double> init_vel = {vx, vy, vz};
 
-    double fx = 1;
-    double fy = 1;
-    double fz = 1;
-    //Run verlet
-    int nb_steps = 100;
-    double timestep = .1;
+    // Run verlet integration for nb_steps
+    int nb_steps = 1000;
+    double timestep = .01;
     for (int i = 0; i < nb_steps; ++i) {
         verlet_step1(x, y, z, vx, vy ,vz, fx, fy, fz, timestep);
         verlet_step2(vx, vy, vz, fx, fy, fz, timestep);
@@ -46,8 +41,8 @@ TEST(VerletTest, AnalyticalNearness) {
     double anal_z = fz * 0.5 * t*t + init_vel[2] * t + init_pos[2];
 
     double anal_vx = fx * t + init_vel[0];
-    double anal_vy = fy * t + init_vel[0];
-    double anal_vz = fz * t + init_vel[0];
+    double anal_vy = fy * t + init_vel[1];
+    double anal_vz = fz * t + init_vel[2];
 
     EXPECT_NEAR(x, anal_x, 1e-6);
     EXPECT_NEAR(y, anal_y, 1e-6);
@@ -55,11 +50,10 @@ TEST(VerletTest, AnalyticalNearness) {
     EXPECT_NEAR(vx, anal_vx, 1e-6);
     EXPECT_NEAR(vy, anal_vy, 1e-6);
     EXPECT_NEAR(vz, anal_vz, 1e-6);
-
-
 }
 
-TEST(VerletMultipleAtoms, AnalyticalNearness) {
+// Multiple atom test
+TEST(VerletTest, MultipleAtom) {
     // Test for multiple atoms using Eigen Arrays
     Positions_t pos, i_pos;
     Velocities_t vel, i_vel;
@@ -76,7 +70,7 @@ TEST(VerletMultipleAtoms, AnalyticalNearness) {
     i_vel = vel;
 
     // Integrate
-    int nb_steps = 1000;
+    int nb_steps = 10000;
     double timestep = .1;
     for (int i = 0; i < nb_steps; ++i) {
         verlet_step1(pos, vel, frc, timestep);
@@ -92,6 +86,7 @@ TEST(VerletMultipleAtoms, AnalyticalNearness) {
     for(int i = 0; i < 3 ; i++){
         for(int j = 0; j < num_atoms; j++){
             EXPECT_NEAR(anal_pos(i, j), pos(i, j), 1e-6);
+            EXPECT_NEAR(anal_vel(i, j), vel(i, j), 1e-6);
         }
     }
 }
